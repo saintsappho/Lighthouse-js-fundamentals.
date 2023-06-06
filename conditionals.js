0cc072c226840b2809d32ef0c2618ed35b5acdd1
Robin Fleur <robinrosefleur@gmail.com>
2023-06-06 04:56:01 +0000

    Conditionals for the weather

---
+++ conditionals.js
@@ -0,0 +1,12 @@
+const temp = 12;
+
+
+if (temp < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temp<15) {
+  console.log("Throw on a sweater!");
+} else {
+  console.log("T-shirt weather!")
+}
+
+console.log("Now you're ready to go outside!");
