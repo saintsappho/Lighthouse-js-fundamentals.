01cec578d029dc5df91a597a665721733a302675
Robin Fleur <robinrosefleur@gmail.com>
2023-06-08 05:37:47 +0000

    shopping list x2, and once in reverse

---
+++ ingredients.js
@@ -0,0 +1,18 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0
+  while(i < ingredients.length){
+    console.log(ingredients[i]);
+    i++
+  }console.log('\n')
+// Write a for loop that prints out the contents of ingredients:
+for (i = 0; i < ingredients.length; i++){
+  console.log(ingredients[i])
+} console.log('\n')
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (i = 0; i < ingredients.length; i++){
+  if (i === 0){
+    ingredients.reverse();
+  } console.log(ingredients[i]);
+}
